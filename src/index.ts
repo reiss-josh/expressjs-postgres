0f2c3a440a3603f661a3d8b756e6f68d2988ff58
reiss-josh <reiss.joshuae@gmail.com>
2022-08-26 15:55:03 -0700

    Updated to support my use case

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,59 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+app.get("/games/:appIDs", async (req, res) => {
+  console.log(req.params.appIDs);
+  const { rows } = await pool.query("SELECT * FROM GAMES WHERE APPID IN "+ req.params.appIDs);
+  console.log(rows);
+  res.send(`${rows}`);
+});
+
+app.post("/games", async (req, res) => {
+  //console.log(req.body);
+  const game = req.body;
+  const query = "INSERT INTO games (appID, name, isMultiplayer, isOnlineMultiplayer, isLocalMultiplayer, isSupportGamepad, isVirtualReality) VALUES(" +
+  game.appID + ',\'' + game.name + '\',\'' + game.isMultiplayer + '\',\'' + game.isOnlineMultiplayer + '\',\'' + game.isLocalMultiplayer + '\',\'' + game.isSupportGamepad + '\',\'' + game.isVirtualReality + "\');"
+  //console.log(query);
+  try{
+    await pool.query(query);
+    res.send('Added new game to DB successfully');
+  } catch (error) {
+    console.log(error);
+    res.send("An error occured.");
+  }
+});
+
+//takes an appid and a parameter, then updates it
+app.put("/games", async (req, res) => {
+  //console.log(req.body);
+  const game = req.body;
+  const query = "UPDATE games SET " +
+    req.body.column + '=\'' + req.body.value +
+    "\' WHERE appid = " + req.body.appID + ";"
+  //console.log(query);
+  try{
+    await pool.query(query);
+    res.send('Updates game in DB successfully');
+  } catch (error) {
+    console.log(error);
+    res.send("An error occured.");
+  }
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+/*
+curl -H "Content-Type: application/json" \
+  --request PUT \
+  -d '{"appID":"421","column":"name","value":"Weed4"}' \
+  http://localhost:3333/games/
+
+curl -H "Content-Type: application/json" \
+  --request POST \
+  -d '{"appID":"421","name":"Weed2","isMultiplayer":"false","isOnlineMultiplayer":"true","isLocalMultiplayer":"false","isSupportGamepad":"true","isVirtualReality":"true"}' \
+  http://localhost:3333/games/
+
+curl 'http://localhost:3333/games/(421,422,5)'
+*/
